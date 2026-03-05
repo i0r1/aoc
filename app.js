cd2b244f6a5f80b6bc6449387f7372dc426584d6
Isidore <iori7891@gmail.com>
2026-03-05 16:17:54 -0700

    added input files

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
+const fs = require("fs");
+const readline = require("readline");
+
 function checkSafe(report) {
   if (report.length < 2) return true;
-  
-  const isIncreasing = report.slice(1).every(
-    (num, i) => num - report[i] > 0 && num - report[i] < 4
-  );
-  const isDecreasing = report.slice(1).every(
-    (num, i) => report[i] - num > 0 && report[i] - num < 4
-  );
+
+  const isIncreasing = report
+    .slice(1)
+    .every((num, i) => num - report[i] > 0 && num - report[i] < 4);
+  const isDecreasing = report
+    .slice(1)
+    .every((num, i) => report[i] - num > 0 && report[i] - num < 4);
   return isIncreasing || isDecreasing;
 }
 
-function main() {
-  const reports = [
-    [7, 6, 4, 2, 1],
-    [1, 2, 7, 8, 9],
-    [9, 7, 6, 2, 1],
-    [1, 3, 2, 4, 5],
-    [8, 6, 4, 4, 1],
-    [1, 3, 6, 7, 9],
-  ];
-
-  reports.forEach(report => {
-    const safe = checkSafe(report);
-    console.log(`${report} ${safe ? 'Safe' : 'Unsafe'}`);
+async function main() {
+  let count = 0;
+  const rl = readline.createInterface({
+    input: fs.createReadStream("input.txt"),
+    crlfDelay: Infinity,
   });
+  
+  for await (const line of rl) {
+    const report = line.split(" ").map(Number);
+    const safe = checkSafe(report);
+    count += safe ? 1 : 0;
+    // console.log(`${report} ${safe ? "Safe" : "Unsafe"}`);
+  }
+  console.log(`Total safe reports: ${count}`);
 }
 
 main();
